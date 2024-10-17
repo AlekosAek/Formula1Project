2d3ae1383c7e6cad0cc807b982f946976efdb0d7
AlekosAek <alexandroslenis@yahoo.com>
2024-10-18 01:33:40 +0200

    add finish

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
-
+import { DriverProvider } from './Driverscontext.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+   <DriverProvider>
+     <RouterProvider router={router} />
+   </DriverProvider>
   </StrictMode>,
 )
